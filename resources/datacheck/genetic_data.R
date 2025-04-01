b9f035d563d6df9c6b76aa184c5e17fd811e02f0
Haotian2020 <er20212@bristol.ac.uk>
2025-04-01 13:36:15 +0100

    add genome build imputation and liftover function

--- resources/datacheck/genetic_data.R
+++ resources/datacheck/genetic_data.R
@@ -2,25 +2,20 @@ errorlist <- list()
 warninglist <- list()
 
 library(data.table)
+library(GwasDataImport)
 suppressMessages(library(matrixStats))
 
 args <- (commandArgs(TRUE))
 bim_file <- as.character(args[1])
-bim_file <- as.character("/user/work/er20212/DEEP_mqtls/input_data/data_filtered.bim")
-
 fam_file <- as.character(args[2])
-fam_file <- as.character("/user/work/er20212/DEEP_mqtls/input_data/data_filtered.fam")
-
 quality_file <- as.character(args[3])
-
 controlsnps_file <- as.character(args[4])
-controlsnps_file <- as.character("/user/work/er20212/DEEP_mqtls/resources/genetics/snpsforbuildandposcheck.txt")
-
 snpsbychr_file <- as.character(args[5])
 snpsbychr_plot <- as.character(args[6])
 cohort_descriptives_file <- as.character(args[7])
 quality_plot <- as.character(args[8])
-ori_build <- as.character(args[9])
+ori_build <- as.numeric(args[9])
+miss_liftover <- as.character(args[10])
 
 message("Checking bim file: ", bim_file)
 controlsnps <- read.table(controlsnps_file, header = F, stringsAsFactors = F)
@@ -55,28 +50,70 @@ if (length(w) > 0) {
 }
 
 message("Checking genome build")
-
+accepted_builds <- c(37, 38)
+# determine the genome build
 build <- GwasDataImport::determine_build_position(pos=bim[, 4], build = c(37, 38, 36))
 
-if (is.na(build) | build == 36) {
-    msg <- paste0("Please check that the bim file is in build 37 or 38. The bim file does not appear to be in any of these builds. \n",
-                  "If you are using build 36, please contact Haotian (haotian.tang@bristol.ac.uk).")
-    errorlist <- c(errorlist, msg)
-    warning("ERROR: ", msg)
+if (is.na(build) || !(build %in% accepted_builds)) {
+  extra_msg <- if (build == 36) {
+    " If you are using build 36, please contact Haotian (haotian.tang@bristol.ac.uk)."
+  } else {
+    ""
+  }
+  msg <- sprintf("Please check that the bim file is in build 37 or 38. The bim file does not appear to be in these builds.%s", extra_msg)
+  errorlist <- c(errorlist, msg)
+  warning("ERROR: ", msg)
 }
 
 if (build != ori_build) {
-    msg <- paste0("Please check that imputed genome build is GRCh", ori_build, ", but as the bim file is GRCh", build)
-    errorlist <- c(errorlist, msg)
-    warning("ERROR: ", msg)
+  msg <- sprintf("Please check your config file and bim file. Imputed genome build is GRCh%d, but the bim file is GRCh%d", ori_build, build)
+  errorlist <- c(errorlist, msg)
+  warning("ERROR: ", msg)
 }
 
-message("Genome build liftover")
 if (build == 37) {
+    message("Genome build liftover")
+    if(mean(grepl("^rs", bim$V3)) > 0.8) {
+        temp_bim = GwasDataImport::liftover_gwas(
+        dat = bim,
+        build = c(37, 38, 36),
+        to = 38,
+        chr_col = "V1",
+        pos_col = "V4",
+        snp_col = "V2",
+        ea_col = "V5",
+        oa_col = "V6")
+    } else {
+        temp_bim = GwasDataImport::liftover_gwas(
+        dat = bim,
+        build = c(37, 38, 36),
+        to = 38,
+        chr_col = "V1",
+        pos_col = "V4",
+        snp_col = NULL,
+        ea_col = "V5",
+        oa_col = "V6")
+    }
+
+    message(paste0("Liftover complete. ", nrow(temp_bim), " SNPs were successfully converted to GRCh38."))
+    message(paste0("Liftover failed for ", nrow(bim) - nrow(temp_bim), " SNPs."))
+
+    if((nrow(temp_bim)/nrow(bim)) < 0.99) {
+        msg <- paste0("Liftover was not successful for more than 1% of the SNPs. Please contact Haotian (haotian.tang@bristol.ac.uk).")
+        errorlist <- c(errorlist, msg)
+        warning("ERROR: ", msg)
+    }
     
+    message("Missing SNPs after liftover saved")
+    write.table(bim[!(bim$V2%in%temp_bim$V2),], file = miss_liftover, sep = "\t", quote = F, row.names = F, col.names = F)
+
 } else if (build == 38) {
-    message()
+    message("Genome build is GRCh38, no liftover required")
 }
+
+temp_bim$V2 <- with(temp_bim, paste0(V1, ":", V4, "_",
+                        ifelse(V5 < V6, paste(V5, V6, sep = "_"), paste(V6, V5, sep = "_"))))
+bim<- temp_bim
 
 message("Checking strand")
 
@@ -126,7 +163,7 @@ if (any(duplicated(bim[, 2]))) {
     warning("ERROR: ", msg)
 }
 
-message("Checking build against control SNPs by checking positions of common controlSNPs")
+message("Checking build against control SNPs by checking positions of common control SNPs")
 no.SNPs.bychr <- NULL
 for (i in 1:22)
 {
@@ -138,7 +175,7 @@ for (i in 1:22)
     message("Chr ", i, " proportion in agreement: ", pos.check)
     no.SNPs.bychr <- append(no.SNPs.bychr, no.SNPs)
     if (pos.check < 0.25) {
-        msg <- paste0("please change positions for chromosome ", i, " to build 37 as less than 25% of common controlsnps are found")
+        msg <- paste0("please change positions for chromosome ", i, " to build 38 as less than 25% of common controlsnps are found")
         errorlist <- c(errorlist, msg)
         warning("ERROR: ", msg)
     }
@@ -153,8 +190,6 @@ barplot(no.SNPs.bychr, main = "no of SNVs by chromosome", xlab = "chromosome", n
 null <- dev.off()
 
 write.table(no.SNPs.bychr, snpsbychr_file, sep = "\t", quote = F, row.names = F, col.names = F)
-
-
 
 ##
 
