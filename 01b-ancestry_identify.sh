c44730340aa872c5f5e8a5d93b81c1222bd398ec
Haotian2020 <er20212@bristol.ac.uk>
2025-07-10 15:18:31 +0100

    use bfiles instead of vcf in hail

--- 01b-ancestry_identify.sh
+++ 01b-ancestry_identify.sh
@@ -6,18 +6,23 @@ set -- $concatenated
 exec &> >(tee ${section_01c_logfile})
 print_version
 
-echo "Converting PLINK files to VCF format"
-plink2 --bfile "${bfile_raw}" \
-	   --export vcf \
-       --out "${vcf_file}"
+# calculate maf from bfile
+echo "Calculating MAF from bfile"
+plink2 --bfile "${bfile}" --freq --out "${section_01_dir}/"
+
+#echo "Converting PLINK files to VCF format"
+#plink2 --bfile "${bfile_raw}" \
+#	   --export vcf \
+#       --out "${vcf_file}"
 
 echo "Running global PCA"
 # global pca plot
 python "${scripts_directory}/resources/datacheck/ancestry_infer.py" \
     "${section_01_dir}/logs_b/hail.log" \
-    "${vcf_file}.vcf" \
-	"${genome_build}" # Use the genome build variable from the config file
+    "${bfile}" \
+	"${genome_build}" \
+    "${study_name}" \
+    "${scripts_directory}"
 
-# local pca plot
-# to be done
+# local pca plot to be done
 echo "Successfully completed script 1b"
