52ae8ab599e2abd005149b49f2860291a69cd4a1
Haotian2020 <er20212@bristol.ac.uk>
2025-07-22 12:36:21 +0100

    test

--- 01b-ancestry_identify.sh
+++ 01b-ancestry_identify.sh
@@ -8,7 +8,7 @@ print_version
 
 # calculate maf from bfile
 echo "Calculating MAF from bfile"
-plink2 --bfile "${bfile}" --freq --out "${section_01_dir}/"
+plink2 --bfile "${bfile_raw}" --freq --out "${section_01_dir}/"
 
 #echo "Converting PLINK files to VCF format"
 #plink2 --bfile "${bfile_raw}" \ 
@@ -19,7 +19,7 @@ echo "Running global PCA"
 # global pca plot
 ${Python_directory}python "${scripts_directory}/resources/datacheck/ancestry_infer.py" \
     "${section_01_dir}/logs_b/hail.log" \
-    "${bfile}" \
+    "${bfile_raw}" \
 	"${genome_build}" \
     "${study_name}" \
     "${scripts_directory}"
