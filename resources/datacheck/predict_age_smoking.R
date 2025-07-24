c87639f273a4ac4a94f88149f1abbc48c08a8e16
Sarah Watkins <36238000+shwatkins@users.noreply.github.com>
2025-07-24 15:57:32 +0100

    Added code for numeric smoking vars

--- resources/datacheck/predict_age_smoking.R
+++ resources/datacheck/predict_age_smoking.R
@@ -4,6 +4,8 @@
 # takes relatively little time to calculate; 
 # and is a first generation clock that tries to predict chronological age.
 
+# QUESTION - check that numerical smoking vars will indeed be called Smoking_numeric
+
 # Args
 arguments <- commandArgs(T)
 
@@ -21,40 +23,34 @@ suppressPackageStartupMessages(library(viridis))
 suppressPackageStartupMessages(library(ggpubr))
 
 message("Reading in data and matching up samples across files")#######################################
-covs <- load(cov_file)
-
-# QUESTION - do we require all DNAm samples to have genetic data?
-# we need to match this up to what happens in check_phenotypes
-# we don't need to load the fam file if we subset the covar file to whichever participants
-# we want in check_phenotypes.R (commonids_mgc line)
-# but equally if not all dnam participants have genetic data, we should remove this subsetting
-fam <- read.table(fam_file)[,c(1,2)]
-covs <- covs[match(fam[,2], covs[,"IID"]),]
-
+load(cov_file)
 load(beta_file)
-norm.beta <- norm.beta[, match(fam[,2], colnames(norm.beta))]
-message(paste(nrow(fam), "samples with genetic data matched to methylation data"))
+participants <- as.character(intersect(colnames(norm.beta),covar$IID))
+covar <- covar[covar$IID%in%participants,]
+norm.beta <- norm.beta[,participants]
+message("Number of samples with covariate and methylation data: ", length(participants))
 
+message(paste(nrow(fam), "samples with genetic data matched to methylation data"))
 
 message("Predicting DNAmAge")#############################################
 
 ret <- meffonym.score(norm.beta, "hannum")
-hannum_cor <- cor(ret$score, pheno_df$Age_numeric)
-message("The correlation between predicted DNAm age and actual age is ", cor(ret$score, pheno_df$Age_numeric))
-message("The r squared between predicted DNAm age and actual age is ", cor(ret$score, pheno_df$Age_numeric)^2)
+hannum_cor <- cor(ret$score, covar$Age_numeric)
+message("The correlation between predicted DNAm age and actual age is ", cor(ret$score, covar$Age_numeric))
+message("The r squared between predicted DNAm age and actual age is ", cor(ret$score, covar$Age_numeric)^2)
 
 # warning if the correlation is lower than 0.5-0.6
 if(hannum_cor<0.6){
-  message("The correlation between predicted DNAm age and actual age is ", cor(ret$score, pheno_df$Age_numeric),
-          "; this seems a little bit low. Is the variance in age low in your dataset? The age range is",range(pheno_df$Age_numeric))
+  message("The correlation between predicted DNAm age and actual age is ", cor(ret$score, covar$Age_numeric),
+          "; this seems a little bit low. Is the variance in age low in your dataset? The age range is",range(covar$Age_numeric))
 }
 message("There were ",length(ret$sites)," DNAm sites used out of a possible 71 for prediction of Hannum age")
 
-pheno_df$Hannum_age <- ret$score
+covar$Hannum_age <- ret$score
 
-hannum_plot <- ggplot(data=pheno_df, aes(x=Age_numeric,y=Hannum_age)) +
+hannum_plot <- ggplot(data=covar, aes(x=Age_numeric,y=Hannum_age)) +
             geom_point(alpha=0.5,colour="#440154FF") +
-            labs(title=paste0(cohort_name," Hannum age vs age, cor =",cor(pheno_df$Age_numeric,ret$score),"p =",signif(cor.test(pheno_df$Age_numeric,ret$score)$p.value),digits=2))+
+            labs(title=paste0(cohort_name," Hannum age vs age, cor =",cor(covar$Age_numeric,ret$score),"p =",signif(cor.test(covar$Age_numeric,ret$score)$p.value),digits=2))+
             geom_smooth(method='lm')+
             theme_minimal()
 
@@ -62,17 +58,27 @@ hannum_plot <- ggplot(data=pheno_df, aes(x=Age_numeric,y=Hannum_age)) +
 message("Predicting smoking")#############################################
 
 ret <- meffonym.score(norm.beta, "mcigarette")
-pheno_df$mcigarette <- ret$score
+covar$mcigarette <- ret$score
 
-# QUESTION - will all smoking be categorical? will some cohorts only have pack years?
-# if so, we will need to add an extra if statement to accommodate this
+# in case some cohorts only have smoking quantity,
+# recode quantity into a factor if they don't have categories (0 = never, 1+= yes)
+
+if ("Smoking_factor" %in% colnames(covar)) {
+  message("Smoking_factor variable already exists.")
+} else if ("Smoking_numeric" %in% colnames(covar)) {
+  # Create Smoking_factor based on Smoking_numeric
+  covar$Smoking_factor <- ifelse(covar$Smoking_numeric == 0, "No", "Yes")
+  message("Smoking_factor variable created based on Smoking_numeric.")
+} else {
+  message("Smoking_numeric column not found. Cannot create Smoking_factor. Please check your phenotype data")
+}
 
 # add in if statement; plot if they have smoking variable
 # if they don't have a smoking variable, plot the distribution of predicted smoking
 
 if ("Smoking_factor" %in% colnames(df)) {
   message("Smoking_factor found in phenotype dataframe: running mcigarette plot of reported smoking vs DNAm-estimated smoking")
-  mcigarette_plot <- ggplot(data=pheno_df, aes(x=Smoking_factor,y=mcigarette,color=Smoking_factor)) +
+  mcigarette_plot <- ggplot(data=covar, aes(x=Smoking_factor,y=mcigarette,color=Smoking_factor)) +
     geom_boxplot()+
     scale_colour_viridis(discrete=T,begin=0,end=0.65)+
     labs(title=paste0(cohort_name," reported vs predicted smoking, p=",signif(wilcox.test(pheno_temp[,i] ~ pheno_temp[,j], data = pheno_temp, alternative = c("two.sided"))$p.value),digits=2))+
@@ -85,9 +91,9 @@ if ("Smoking_factor" %in% colnames(df)) {
   print("Smoking_factor not found in phenotype dataframe (if this is a measure you should have, please check the name of the smoking variable)
         :running mcigarette plot of DNAm-estimated smoking")
   mcigarette_plot <- ggplot() +
-    geom_density(data=pheno_df, aes(x=pheno_df$mcigarette), colour="#1F968BFF")+
-    labs(title=paste0(i,":n of NAs=",sum(is.na(pheno_df$mcigarette))))+#,color="Legend")+
-    geom_vline(xintercept = mean(pheno_df$mcigarette))+
+    geom_density(data=covar, aes(x=covar$mcigarette), colour="#1F968BFF")+
+    labs(title=paste0(i,":n of NAs=",sum(is.na(covar$mcigarette))))+#,color="Legend")+
+    geom_vline(xintercept = mean(covar$mcigarette))+
     theme_minimal()
   
 }
