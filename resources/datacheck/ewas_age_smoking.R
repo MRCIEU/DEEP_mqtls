f99b61e55d225dacb8be900a1c9eb4bb049f7683
Sarah Watkins <36238000+shwatkins@users.noreply.github.com>
2025-07-25 10:36:41 +0100

    Added covariates, summary stats, smoking variable coding

--- resources/datacheck/ewas_age_smoking.R
+++ resources/datacheck/ewas_age_smoking.R
@@ -1,9 +1,13 @@
 # age and smoking EWAS
 # QC phase 1
 # checking for EWAS signal associated with age and smoking
-# QUESTION - what are we adjusting for in these EWAS? Are we including cell counts?
-# QUESTIOn - how are we coding smoking for child cohorts?
+# QUESTION - how are we coding smoking for child cohorts?
+  # this will be maternal smoking (will flag different sites - add to check specific ones in output)
+  # we can add an if statement looking for the maternal smoking var
 # QUESTION - are there any cohorts that may be newborns with little maternal smoking? if so we may need a different approach
+  # this could maybe be done by checking epigenetic age looks reasonable instead of these EWAS
+  # cohorts to check: check senegal and india (mumbai) studies. and ?malawi. they probs have smoking data?
+  # if they don't have smoking data then maybe we can do an if statement in the bash script and run a different R script
 
 # Args
 arguments <- commandArgs(T)
@@ -11,42 +15,50 @@ arguments <- commandArgs(T)
 beta_file <- arguments[1]
 cov_file <- arguments[2] # [winsorized_covariates_file from check_phenotypes]
 meth_array <- arguments[3]
-  # QUESTION - do we need cell counts here?
-#cellcount_file <- arguments[9] 
-study_name <- arguments[x]
-smoking_ewas_stats <- arguments[x]
-smoking_ewas_report <- arguments[x]
-age_ewas_stats <- arguments[x]
-age_ewas_report <- arguments[x]
+cellcount_file <- arguments[4] 
+study_name <- arguments[5]
+study_specific_vars <- arguments[6]
+smoking_ewas_stats <- arguments[7]
+smoking_ewas_report <- arguments[8]
+age_ewas_stats <- arguments[9]
+age_ewas_report <- arguments[10]
 
 suppressPackageStartupMessages(library(meffil))
 
 message("Reading in data and matching up samples across files")#######################################
-covs <- load(cov_file)
-
-# QUESTION - do we require all DNAm samples to have genetic data?
-# we need to match this up to what happens in check_phenotypes
-# we don't need to load the fam file if we subset the covar file to whichever participants
-# we want in check_phenotypes.R (commonids_mgc line)
-# but equally if not all dnam participants have genetic data, we should remove this subsetting
-fam <- read.table(fam_file)[,c(1,2)]
-covs <- covs[match(fam[,2], covs[,"IID"]),]
-
+load(cov_file)
 load(beta_file)
-norm.beta <- norm.beta[, match(fam[,2], colnames(norm.beta))]
-message(paste(nrow(fam), "samples with genetic data matched to methylation data"))
+load(cellcount_file)
+participants <- as.character(intersect(colnames(norm.beta),covar$IID))
+covar <- covar[covar$IID%in%participants,]
+norm.beta <- norm.beta[,participants]
+
+celltypes <- colnames(cellcount_file) # replace this - what is the name of the cell counts df??
+# also is IID a column in the cellcounts file?
 
 message("Setting up EWAS")#######################################
 
 # 1. smoking ewas
 
-# QUESTION - will all smoking be categorical? will some cohorts only have pack years?
-# if so, we will need to add an extra if statement to accomodate this
+# in case some cohorts only have smoking quantity,
+# recode quantity into a factor if they don't have categories (0 = never, 1+= yes)
+  # QUESTION - check that numerical smoking vars will indeed be called Smoking_numeric
+  # rather than pack-years or something
+
+if ("Smoking_factor" %in% colnames(covar)) {
+  message("Smoking_factor variable already exists.")
+} else if ("Smoking_numeric" %in% colnames(covar)) {
+  # Create Smoking_factor based on Smoking_numeric
+  covar$Smoking_factor <- ifelse(covar$Smoking_numeric == 0, "No", "Yes")
+  message("Smoking_factor variable created based on Smoking_numeric.")
+} else {
+  message("Smoking_numeric column not found. Cannot create Smoking_factor. Please check your phenotype data")
+}
 
 # QUESTION - is there a config file option that says what array the methylation data is on?
-# if so we'll pull that in here:
+featureset <- meffil:::guess.featureset(rownames(norm.beta))
 ewas_threshold <- ifelse(meth_array == '450k', 2.4e-7,
-                ifelse(meth_array == 'EPIC', 9e-8, NA))
+                ifelse(meth_array %in% c('epic', 'epic2'), 9e-8, NA))
 if(is.na(ewas_threshold)){
   message("ERROR: No EWAS threshold has been selected as the methylation array type does
           not match the expected values of 450k or EPIC. Please check your config file")
@@ -58,10 +70,9 @@ ewas.parameters <- meffil.ewas.parameters(sig.threshold=ewas_threshold,  ## EWAS
                                           qq.inflation.method="median",  ## measure inflation using median
                                           model="all") ## select default EWAS model; 
 
-# QUESTION - which covars do we want for the adjusted model?
-ewas_covars_smoking <- c("Age_numeric","Sex_factor") # need to updat these var names
-# QUESTION : might we need to stratify smoking EWAS by sex in some cohorts?
-# could that look something like if 4x as many men smoke (for example)
+ewas_covars_smoking <- c("Age_numeric","Sex_factor",celltypes,study_specific_vars) # need to update these var names?
+# QUESTION : might we need to stratify smoking EWAS by sex in some cohorts? - 
+  # answer:No not at the moment
 
 message("Starting smoking EWAS")#######################################
 
@@ -70,10 +81,13 @@ if ("Smoking_factor" %in% colnames(df)) {
   participants <- as.character(covs$IID)
   meth.temp <- norm.beta[,participants]
   ewas.smoking <- meffil.ewas(meth.temp, variable=pheno$Smoking.factor, covariates=pheno.temp[,ewas_covars_smoking], sva=T, isva=F, random.seed=23) 
-  # QUESTION - do we want to save out the ewas summary stats? We might only want the report?
-  save(ewas.smoking, file=paste0(smoking_ewas_stats,"_",study_name,".Robj"))
+  # save out the ewas summary stats:
+  ewas.out <- ewas.smoking$analyses
+  save(ewas.out, file=paste0(smoking_ewas_stats,"_",study_name,".Robj"))
+  # generate and save html report of EWAS:
   ewas.summary<-meffil.ewas.summary(ewas.smoking,meth.temp,parameters=ewas.parameters)                              
   meffil.ewas.report(ewas.summary, output.file=paste0(smoking_ewas_report,"_",study_name,".html"))
+  # let's have a quick glance at how many hits we get:
   hits <- ewas.smoking$analyses$none$table
   message("There were",nrow(hits[hits$p.value<ewas_threshold,]),"DNAm sites associated with smoking in the unadjusted model")
   hits <- ewas.smoking$analyses$all$table
@@ -81,8 +95,8 @@ if ("Smoking_factor" %in% colnames(df)) {
   
 } else {
   # Run this command if 'Smoking_factor' column does not exist
-  print("Smoking_factor not found in phenotype dataframe (if this is a measure you should have, 
-  please check the name of the smoking variable)")
+  print("Smoking_factor not found in phenotype dataframe. If you study has smoking data,
+  please ensure your phenotype dataframe has either Smoking_factor or Smoking_numeric")
 
 }
 
@@ -91,13 +105,13 @@ if ("Smoking_factor" %in% colnames(df)) {
 
 message("Starting age EWAS")#######################################
 
-ewas_covars_age <- c("Sex_factor") # need to update these var names
+ewas_covars_age <- c("Sex_factor",celltypes,study_specific_vars) # need to update these var names
 participants <- as.character(covs$IID)
 meth.temp <- norm.beta[,participants]
 
 ewas.age <- meffil.ewas(meth.temp, variable=pheno$Smoking.factor, covariates=pheno.temp[,ewas_covars_age], sva=T, isva=F, random.seed=23) 
-# QUESTION - do we want to save out the ewas summary stats? We might only want the report?
-save(ewas.age, file=paste0(age_ewas_stats,"_",study_name,".Robj"))
+ewas.out <- ewas.age$analyses
+save(ewas.out, file=paste0(age_ewas_stats,"_",study_name,".Robj"))
 ewas.summary<-meffil.ewas.summary(ewas.age,meth.temp,parameters=ewas.parameters)                              
 meffil.ewas.report(ewas.summary, output.file=paste0(age_ewas_report,"_",study_name,".html"))
 
