09a627dc0eae8a4b61f13cfeff9d85bc287af437
Haotian2020 <er20212@bristol.ac.uk>
2025-07-28 15:23:19 +0100

    add ancestry into easyqc

--- 01b-ancestry_identify.sh
+++ 01b-ancestry_identify.sh
@@ -37,6 +37,7 @@ plink2 --bfile "${bfile_raw}" \
        --freq \
        --out "${section_01_dir}/"
 
+
 # check against with reference panel using EasyQC
 if [ -f "processed_data/genetic_data/easyQC_hrc.ecf.out" ]
 then
@@ -46,6 +47,40 @@ then
 else
 	echo "passed file check"
 fi
+
+if [[ ${ancestry} == "EUR" || ${ancestry} == "AFR" || ${ancestry} == "AMR" || ${ancestry} == "EAS" || ${ancestry} == "SAS" ]]; then
+    cp ${scripts_directory}/resources/genetics/1000G_${ancestry}_p3v5.TOPMed_Imputed.Allele_Freq.hg38.txt.gz ${home_directory}/processed_data/genetic_data/
+
+else if [[ ${ancestry} == "None" ]]; then
+    echo "No ancestry specified, using all population of topmed snplist and allele frequencies"
+    cp ${scripts_directory}/resources/genetics/topmed.GRCh38.f8wgs.pass.nodup.mac5.maf001.tab.snplist.gz ${home_directory}/processed_data/genetic_data/
+fi
+
+replacement_text="DEFINE --pathOut "${home_directory}"/processed_data/genetic_data"
+awk 'NR==3 {$0 = "'"$replacement_text"'"} 1' ${easyQCscript} > ${easyQCscript%.ecf}_edit.ecf
+${R_directory}Rscript ./resources/genetics/easyQC.R ${bfile}.afreq ${easyQC} ${easyQCfile} ${easyQCscript%.ecf}_edit.ecf
+
+rm ${home_directory}/processed_data/genetic_data/topmed.GRCh38.f8wgs.pass.nodup.mac5.maf001.tab.snplist.gz
+
+mv ${home_directory}/processed_data/genetic_data/easyQC_hrc_edit.multi.AFCHECK.png ${home_directory}/results/02/easyQC_hrc.multi.AFCHECK.png
+mv ${home_directory}/processed_data/genetic_data/easyQC_hrc_edit.rep ${home_directory}/results/02/easyQC_hrc.rep
+
+# Remove mismatched SNPs and flip misaligned SNPs
+echo "Remove mismatched SNPs and NO FLIPPING"
+
+${plink2} \
+	--bfile ${bfile} \
+	--exclude ${easyQC}.mismatch_afcheck.failed.SNPs.txt \
+	--make-bed \
+	--out ${bfile}1 \
+	--threads ${nthreads}
+
+mv ${bfile}1.bed ${bfile}.bed
+mv ${bfile}1.bim ${bfile}.bim
+mv ${bfile}1.fam ${bfile}.fam
+
+
+
 
 echo "Running global PCA"
 # global pca plot
