23721105393ef33e0eba2e630bb4098c713d04b2
Haotian2020 <er20212@bristol.ac.uk>
2025-08-04 11:14:50 +0100

    add liftover map file

--- resources/datacheck/liftover.R
+++ resources/datacheck/liftover.R
@@ -2,6 +2,7 @@ errorlist <- list()
 warninglist <- list()
 
 library(data.table)
+library(dplyr)
 library(GwasDataImport)
 library(rtracklayer)
 
@@ -9,9 +10,15 @@ args <- (commandArgs(TRUE));
 bim_file <- as.character(args[1]);
 ori_build <- as.numeric(args[2]);
 miss_liftover <- as.character(args[3])
+liftover_map <- as.character(args[4])
+section_01_dir <- as.character(args[5])
 
+# bim_file <- "/user/work/er20212/DEEP_mqtls/input_data/data_filtered"
+# ori_build <- 37
 message("Loading bim file: ", bim_file)
 bim <- as.data.frame(fread(paste0(bim_file, ".bim")))
+# miss_liftover <- "./results/01/miss_liftover.txt"
+# liftover_map <- "./processed_data/genetic_data/liftover_map.txt"
 
 message("Number of SNPs: ", nrow(bim))
 
@@ -35,6 +42,9 @@ if (mean(startsWith(bim[, 2], "rs")) >= 0.8) {
   )
 }
 
+write.table(build, file = paste0(section_01_dir, "/inferred_build.txt"),
+  sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
 if (is.na(build) || !(build %in% accepted_builds)) {
   extra_msg <- if (build == 36) {
     " If you are using build 36, please contact Haotian (haotian.tang@bristol.ac.uk)."
@@ -47,7 +57,7 @@ if (is.na(build) || !(build %in% accepted_builds)) {
 }
 
 if (build != ori_build) {
-  msg <- sprintf("Please check your config file and bim file. Imputed genome build is GRCh%d, but the bim file is GRCh%d", build, ori_build)
+  msg <- sprintf("Please confirm your genome build. Imputed genome build is GRCh%d, but the bim file is GRCh%d", build, ori_build)
   errorlist <- c(errorlist, msg)
   warning("ERROR: ", msg)
 }
@@ -55,6 +65,8 @@ if (build != ori_build) {
 if (build == 37) {
     message("Genome build liftover")
     if(mean(grepl("^rs", bim$V2)) >= 0.8) {
+      # check if the bim file has rsID
+      message("Detected rsID format, using liftover_gwas with snp_col")
         temp_bim = GwasDataImport::liftover_gwas(
         dat = bim,
         build = c(37, 38, 36),
@@ -65,6 +77,7 @@ if (build == 37) {
         ea_col = "V5",
         oa_col = "V6")
     } else {
+      message("Detected chr:pos format, using liftover_gwas without snp_col")
         temp_bim = GwasDataImport::liftover_gwas(
         dat = bim,
         build = c(37, 38, 36),
@@ -75,7 +88,7 @@ if (build == 37) {
         ea_col = "V5",
         oa_col = "V6")
     }
-
+    # temp_bim format: chr, ori_id, V3, new_pos, ref, alt
     message(paste0("Liftover complete. ", nrow(temp_bim), " SNPs were successfully converted to GRCh38."))
     message(paste0("Liftover failed for ", nrow(bim) - nrow(temp_bim), " SNPs."))
 
@@ -85,18 +98,22 @@ if (build == 37) {
         warning("ERROR: ", msg)
     }
 
-    message("Saving the missing SNPs during liftover")
+    missing_snps <- bim[!(bim$V2 %in% temp_bim$V2), "V2"]
+    if (nrow(as.data.frame(missing_snps)) > 0) {
+      message("Save the missing SNPs during liftover")
+      write.table(missing_snps, file = miss_liftover, sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+      message("Missing SNPs written to file: ", miss_liftover)
+      } else {
+        message("No missing SNPs during liftover.")
+        }
 
-    write.table(bim[!(bim$V2%in%temp_bim$V2),"V2"], file = miss_liftover, sep = "\t", quote = F, row.names = F, col.names = F)
+    message("Save the map file")
 
-    temp_bim$V2 <- with(temp_bim, paste0(V1, ":", V4, "_",
-                        ifelse(V5 < V6, paste(V5, V6, sep = "_"), paste(V6, V5, sep = "_"))))
-    bim <- temp_bim
-    
-    bim[["V1"]][bim[["V1"]] == "X"] <- "23"
-    bim[["V1"]][bim[["V1"]] == "Y"] <- "24"
-    
-    write.table(bim, file = paste0(bim_file,"_liftover.bim"), sep = "\t", quote = F, row.names = F, col.names = F)
+    map <- left_join(temp_bim, bim, by = "V2")
+    map_select <- map[, c(2,4)]
+    colnames(map_select) <- c("OLD_SNP", "NEW_POS")
+
+    write.table(map_select, file = liftover_map, sep = "\t", quote = F, row.names = F, col.names = T)
 
     } else if (build == 38) {
 
