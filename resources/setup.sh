b57369036fe0c59e8d6bfb54d61c1eb6d75fb77b
Haotian2020 <er20212@bristol.ac.uk>
2025-08-27 12:12:08 +0100

    move env activation to end

--- resources/setup.sh
+++ resources/setup.sh
@@ -6,6 +6,36 @@ git pull
 # Initialize variables
 config_file="./config"
 
+# Parse options using getopts
+while getopts "c:" opt; do
+    case $opt in
+        c) config_file=$OPTARG ;;
+        *) echo "Usage: $0 -c <config_file>"
+           exit 1 ;;
+    esac
+done
+
+# Shift option arguments, so $1 becomes the first positional argument
+shift $((OPTIND - 1))
+
+# Initialize an empty string
+concatenated=""
+
+# Loop through all arguments
+for arg in "$@"; do
+    concatenated="$concatenated$arg "
+done
+
+
+set -e
+echo "-----------------------------------------------"
+echo ""
+echo "Using config located at:" ${config_file}
+echo ""
+echo "-----------------------------------------------"
+
+source ${config_file}
+
 # Activate the environment
 if [ -z "$R_directory" ] && [ -z "$Python_directory" ] && [ -z "$Python2_directory" ]; then
     if command -v mamba &> /dev/null; then
@@ -49,33 +79,3 @@ export RUN_CMD
 export R_directory
 export Python_directory
 export Python2_directory
-
-# Parse options using getopts
-while getopts "c:" opt; do
-    case $opt in
-        c) config_file=$OPTARG ;;
-        *) echo "Usage: $0 -c <config_file>"
-           exit 1 ;;
-    esac
-done
-
-# Shift option arguments, so $1 becomes the first positional argument
-shift $((OPTIND - 1))
-
-# Initialize an empty string
-concatenated=""
-
-# Loop through all arguments
-for arg in "$@"; do
-    concatenated="$concatenated$arg "
-done
-
-
-set -e
-echo "-----------------------------------------------"
-echo ""
-echo "Using config located at:" ${config_file}
-echo ""
-echo "-----------------------------------------------"
-
-source ${config_file}
