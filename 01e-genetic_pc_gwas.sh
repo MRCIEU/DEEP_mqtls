884409b2a542be87fb729f56318b907fad335709
Haotian2020 <er20212@bristol.ac.uk>
2025-08-28 14:00:39 +0100

    1st version pc gwa

---
+++ 01e-genetic_pc_gwas.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+source resources/setup.sh "$@"
+set -- $concatenated
+
+exec &> >(tee ${section_01e_logfile})
+print_version
+
+echo "Running GWAS for genetic PCs (PC1-PC10)"
+
+for pc in {1..10}; do
+    pc_col="PC${pc}"
+    pheno_file="genetic_pc_gwas.PC${pc}.pheno"
+
+    awk -v col="$((pc+2))" 'NR==1{next} {print $1, $2, $col}' ${genetic_pc_gwas} > ${pheno_file}
+
+    echo "Running GWAS for ${pc_col}"
+    if [ "${related}" = "yes" ]; then
+        ${gcta} \
+            --bfile ${bfile} \
+            --grm-sparse ${grmfile_fast}_rel \
+            --fastGWA-mlm \
+            --pheno ${pheno_file} \
+            --qcovar ${qcovar_file} \
+            --covar ${qcovar_noPC_file} \
+            --out ${section_01_dir}/gwas_${pc_col} \
+            --thread-num ${nthreads}
+
+    elif [ "${related}" = "no" ]; then
+        ${gcta} \
+            --bfile ${bfile} \
+            --grm-sparse ${grmfile_fast}_unrel \
+            --fastGWA-mlm \
+            --pheno ${pheno_file} \
+            --qcovar ${qcovar_file} \
+            --covar ${qcovar_noPC_file} \
+            --out ${section_01_dir}/gwas_${pc_col} \
+            --thread-num ${nthreads}
+    fi
+
+    tr -s " " < ${section_01_dir}/gwas_${pc_col}.fastGWA | gzip -c > ${section_01_dir}/gwas_${pc_col}.fastGWAmlm.gz
+    rm ${section_01_dir}/gwas_${pc_col}.fastGWA
+
+    echo "make manhattan and qq plots for genetic PC ${pc_col}"
+    echo ${section_01_dir}/gwas_${pc_col}.fastGWAmlm.gz > ${section_01_dir}/gwas_${pc_col}.file.txt
+    
+    ${R_directory}Rscript resources/genetics/gcta_plot_gwas.R \
+        ${section_01_dir}/gwas_${pc_col}.file.txt \
+            10 \
+            1 \
+            3 \
+            2 \
+            TRUE \
+            ${positive_control_snp_chr} \
+            ${positive_control_snp_pos} \
+            ${positive_control_snp_window} \
+            ${positive_control_threshold}
+
+done
+
+echo "Successfully completed script 01e"
