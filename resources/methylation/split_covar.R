a6587866261bbb1e8554a3793833c12c06eacfb0
Haotian2020 <er20212@bristol.ac.uk>
2025-08-28 11:25:24 +0100

    rm col without variation

--- resources/methylation/split_covar.R
+++ resources/methylation/split_covar.R
@@ -10,6 +10,10 @@ comb_cov_file <- arguments[1]
 fam_file <- arguments[2]
 ccovar_file <- arguments[3]
 qcovar_file <- arguments[4]
+scripts_directory <- arguments[5]
+genetic_pc_gwas <- arguments[6]
+
+source(paste0(scripts_directory,"/resources/datacheck/fn_rm_constant_col.R"))
 
 # comb_cov file should contain all covariates for mqtl analysis
 # including, IID, cell counts prediction, genetic 20 PCs, Age_numeric Sex_factor Slide_factor predicted_smoking
@@ -31,6 +35,17 @@ quant_cols[names(merged) %in% force_category] <- FALSE
 
 quant_cov <- merged[, c("FID", "IID", names(merged)[quant_cols & !(names(merged) %in% c("FID", "IID"))])]
 category_cov <- merged[, c("FID", "IID", names(merged)[category_cols & !(names(merged) %in% c("FID", "IID"))])]
+
+quant_cov <- remove_constant_cols(quant_cov, "quant_cov")
+category_cov <- remove_constant_cols(category_cov, "category_cov")
+
+# Check that at least one covariate column remains (besides FID and IID)
+if (ncol(quant_cov) < 3) {
+  stop("After removing constant columns, quant_cov only contains FID and IID. Please check your input data.")
+}
+if (ncol(category_cov) < 3) {
+  stop("After removing constant columns, category_cov only contains FID and IID. Please check your input data.")
+}
 
 write.table(quant_cov, file=qcovar_file, sep = "\t", row.names = FALSE, quote = FALSE)
 write.table(category_cov, file=ccovar_file, sep = "\t", row.names = FALSE, quote = FALSE)
