da4098bb732138971632725ffbba30bf0c9714c4
Haotian2020 <er20212@bristol.ac.uk>
2025-09-08 14:32:41 +0100

    start from idat file; save qc.obj in home; shrink.qc.obj assumed to be in input_data

--- resources/methylation/Func_normalization_bw_datasets.R
+++ resources/methylation/Func_normalization_bw_datasets.R
@@ -6,26 +6,30 @@
 suppressPackageStartupMessages(library(meffil))
 
 arguments <- commandArgs(T);
-betas <- arguments[1];
-qc_input_file <- arguments[2];
-study_name <- arguments[3];
-output_path <- arguments[4];
-stage <- arguments[5];
+idat_input_folder <- arguments[1];
+study_name <- arguments[2];
+home_path <- arguments[3];
+stage <- arguments[4];
 
 # qc stage 1 shrink ======= remove unnecessary information from qc objects
 
-if (stage == "shrink" && betas == 0) {
-required_var <- c("controls", "quantiles")
+if (stage == "shrink") {
 
-vars_before <- ls()
-load(qc_input_file)
-vars_after <- ls()
-new_vars <- setdiff(vars_after, vars_before)
-message("Loaded variable(s) from qc.object: ", paste(new_vars, collapse = ", "))
+    vars_before <- ls()
+    load(idat_input_folder)
+    vars_after <- ls()
+    new_vars <- setdiff(vars_after, vars_before)
+    message("idat files folder: ", paste(new_vars, collapse = ", "))
 
-qc_objects_shrink <- meffil.shrink.qc.object(get(new_vars[1]), keep.vars = required_var)
+    samplesheet <- meffil.create.samplesheet(idat_input_folder, recursive=TRUE)
 
-save(qc_objects_shrink, file=paste0(output_path, "/",study_name, "qc_objects_shrink.rda"))
+    qc.objects <- meffil.qc(samplesheet, verbose=TRUE)
+
+    qc_objects_shrink <- meffil.shrink.qc.object(get(new_vars[1]))
+
+    # save original qc.objects to home directory
+    save(qc.objects, file=paste0(home_path, "/processed_data/methylation_data/",study_name, "qc_objects.rda"))
+    save(qc_objects_shrink, file=paste0(home_path, "/results/01/", study_name, "qc_objects_shrink.rda"))
 
 message("Shrunk QC objects created")
 
@@ -37,25 +41,28 @@ message("Shrunk QC objects created")
 # norm.objects = meffil.normalize.quantiles(qc.objects, number.pcs=...)
 # cohort1.norms = norm.objects[names(cohort1.objects)] ...
 # send back shrunk objects after normalization in DEEP
-# format: study__name + "objects.rda"
+# format: study__name + "_objects.rda"
 
 # qc stage 2.  ========= restore information
 
-if (stage == "expand" && betas != 0) {
+load_new_var <- function(file) {
+  vars_before <- ls()
+  load(file)
+  vars_after <- ls()
+  new_vars <- setdiff(vars_after, vars_before)
+  if (length(new_vars) == 0) stop("No new variable loaded from ", file)
+  get(new_vars[1])
+}
 
-    vars_before <- ls()
-    load(qc_input_file)
-    load(paste0(output_path, "/",study_name, "objects.rda"))
-    vars_after <- ls()
-    new_vars <- setdiff(vars_after, vars_before)
-
-    qc.object.ori = get(new_vars[1])
-    dat.norms = get(new_vars[2])
-
-    norm.objects = meffil.expand.norm.object(dat.norms, qc.object.ori)
-    
-    save(norm.objects, file=paste0(output_path, "/processed_data/",study_name, "_harmonized_meth.rda"))
-
-    message("Information restored")
-
+if (stage == "expand") {
+  message("Loading original qc.objects")
+  qc.object.ori <- load_new_var(paste0(home_path, "/processed_data/methylation_data/", study_name, "qc_objects.rda"))
+  
+  message("Loading normalized shrunk objects")
+  message("Please put the file from DEEP server in the input folder")
+  dat.norms <- load_new_var(paste0(home_path, "/input_data/", study_name, "_objects.rda"))
+  
+  norm.objects <- meffil.expand.norm.object(dat.norms, qc.object.ori)
+  save(norm.objects, file = paste0(home_path, "/processed_data/", study_name, "_harmonized_meth.rda"))
+  message("Information restored")
 }
