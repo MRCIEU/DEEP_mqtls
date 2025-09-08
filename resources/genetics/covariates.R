b3cc1c7ef3e479db9fa1ebff9ba2ca7247ee9cc8
Haotian2020 <er20212@bristol.ac.uk>
2025-09-08 14:34:03 +0100

    correct colnames from pred_smk file

--- resources/genetics/covariates.R
+++ resources/genetics/covariates.R
@@ -1,9 +1,7 @@
 # Create covariates file
-
 # Original covariates
 # Genetic PCs
 # Cell counts
-
 
 arguments <- commandArgs(T)
 
@@ -17,8 +15,8 @@ out_file <- arguments[6]
 pca <- read.table(pca_file)[,-1]
 names(pca) <- c("IID", paste("genetic_pc", 1:(ncol(pca)-1), sep=""))
 
-if(cov_file=="NULL")
-{
+if(cov_file=="NULL"){
+  message("No covariate file provided")
 	covs <- data.frame(IID=pca$IID)
 } else {
 	covs <- read.table(cov_file, header=TRUE, stringsAsFactors=TRUE, colClass=c("Sex_factor"="character"))
@@ -27,28 +25,36 @@ if(cov_file=="NULL")
 # Remove FID column if it is present
 if("FID" %in% names(covs)) covs <- subset(covs, select=-c(FID))
 
-smok <- read.table(smoking_file, he=T)[,c('IID', 'Smoking')]
+smok <- read.table(smoking_file, he=T)[,c('IID', 'p_smoking_mcigarette')]
 
 if(cellcount_file=="NULL")
 {
   allcovs <- merge(pca, covs, by="IID", all=TRUE)
   allcovs <- merge(allcovs, smok, by="IID", all=TRUE) 
+  message("No cell count file provided")
+  stop("Cell counts are required for mQTL analysis")
 
 } else {
+
   cellcount <- read.table(cellcount_file, header=TRUE)
+  print(colnames(cellcount))
 
-#cellcount <- cellcount[,-ncol(cellcount)]
+  # Check for unexpected columns
+  if (length(grep("lym", tolower(colnames(cellcount)))) > 0) {
+    stop("There might be unexpected columns in cell count file. Please check the file format.")
+  }
 
-covs <- subset(covs, IID %in% cellcount$IID)
+  covs <- subset(covs, IID %in% cellcount$IID)
 
-allcovs <- merge(cellcount, pca, by="IID", all=TRUE)
-allcovs <- merge(allcovs, covs, by="IID", all=TRUE)
-allcovs <- merge(allcovs, smok, by="IID", all=TRUE)
+  allcovs <- merge(cellcount, pca, by="IID", all=TRUE)
+  allcovs <- merge(allcovs, covs, by="IID", all=TRUE)
+  allcovs <- merge(allcovs, smok, by="IID", all=TRUE)
 }
 
 fam<-read.table(fam_file)
 m<-match(fam[,2],allcovs[,1])
 allcovs<-data.frame(FID=fam[,2],allcovs[m,])
+
 write.table(allcovs,file=paste(out_file,".plink",sep=""),row.names=F,col.names=F,quote=F)
 write.table(allcovs[,-1], file=paste0(out_file, ".txt"), row=F, col=T, qu=F)
 
