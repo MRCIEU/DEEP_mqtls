c329fc01c33209be02e1d238fe391c2e8d60d83e
Haotian2020 <er20212@bristol.ac.uk>
2025-09-09 12:33:35 +0100

    add head

--- resources/genetics/check_positive_controls.R
+++ resources/genetics/check_positive_controls.R
@@ -5,8 +5,7 @@ filt_pos_ctr_file <- arguments[2]
 com_id <- arguments[3]
 
 library(data.table)
-
-pos_ctr_dt <- fread(pos_ctr_file)
+pos_ctr_dt <- fread(pos_ctr_file, header = TRUE)
 com_probe_id <- fread(com_id)
 
 print("Check cpg sites from positive control file are all from common probe IDs")
@@ -14,9 +13,12 @@ print("Check cpg sites from positive control file are all from common probe IDs"
 if (all(pos_ctr_dt$positive_control_cpg %in% com_probe_id$Probe_ID)) {
   print("All cpg sites are from common probe IDs")
   filt_pos_ctr_dt <- pos_ctr_dt
+
 } else {
+
   print("Remove cpg sites are not from common probe IDs")
   filt_pos_ctr_dt <- pos_ctr_dt[pos_ctr_dt$positive_control_cpg %in% com_probe_id$Probe_ID, ]
+
 }
 
 print("Remove NAs from positive control data")
