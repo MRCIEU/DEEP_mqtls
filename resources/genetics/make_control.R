ada9f60b2e1e3dbd03051adf4e4f17bfe7f8dded
Haotian2020 <er20212@bristol.ac.uk>
2025-09-09 12:34:16 +0100

    rewrite make_ctr

--- resources/genetics/make_control.R
+++ resources/genetics/make_control.R
@@ -1,21 +1,14 @@
+library(data.table)
 arguments <- commandArgs(T)
 
-in_file <- arguments[1]
-ids_file <- arguments[2]
-out_file <- arguments[3]
+in_file <- arguments[1];
+ids_file <- arguments[2];
+out_file <- arguments[3];
 
-a <- scan(in_file, nlines=1, what=character(),sep=",")
-b <- scan(in_file, skip=1, nlines=1, what=character(),sep=",")
-b <- as.numeric(b[-1])
-a <- as.character(a[-1])
+a <- fread(in_file, header=T)
+ids <- fread(ids_file, stringsAsFactors=FALSE)
+colnames(ids) <- c("FID", "IID")
+ids <- ids[match(a$IID, ids$IID), ]
+merge <- merge(ids, a, by.x="IID", by.y="IID", sort=F)
 
-#a <- read.table(in_file,he=T)
-ids <- read.table(ids_file, stringsAsFactors=FALSE)
-#ids <- ids[match(a$IID, ids$V2), ]
-ids <- ids[match(a, ids$V2), ]
-#stopifnot(all(ids$V2 == a$IID))
-stopifnot(all(ids$V2 == a))
-#ids$phen <- a[,2]
-ids$phen <- b
-
-write.table(ids, file=out_file, row=F, col=F, qu=F)
+write.table(merge, file=out_file, row=F, col=T, qu=F)
