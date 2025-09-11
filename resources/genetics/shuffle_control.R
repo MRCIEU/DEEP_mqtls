fb6d4e2083c4d557c1968449b24875e0920887f2
Haotian2020 <er20212@bristol.ac.uk>
2025-09-11 16:30:40 +0100

    correct shuffle

--- resources/genetics/shuffle_control.R
+++ resources/genetics/shuffle_control.R
@@ -7,7 +7,5 @@ set.seed(seed)
 df <- read.csv(input_file, header = TRUE, sep = "\t")
 header <- colnames(df)
 df_shuffled <- df
-for (i in 1:nrow(df)) {
-  df_shuffled[i, 2:ncol(df)] <- sample(df[i, 2:ncol(df)])
-}
+df_shuffled[, 2] <- sample(df[, 2])
 write.csv(df_shuffled, output_file, row.names=FALSE, quote=FALSE)
