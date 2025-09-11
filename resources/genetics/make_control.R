88266196e3f23592fa466b2f3763f55136d4ec00
Haotian2020 <er20212@bristol.ac.uk>
2025-09-11 16:29:24 +0100

    order FID IID cpg

--- resources/genetics/make_control.R
+++ resources/genetics/make_control.R
@@ -6,9 +6,13 @@ ids_file <- arguments[2];
 out_file <- arguments[3];
 
 a <- fread(in_file, header=T)
+cpg_id = colnames(a)[2]
 ids <- fread(ids_file, stringsAsFactors=FALSE)
 colnames(ids) <- c("FID", "IID")
 ids <- ids[match(a$IID, ids$IID), ]
 merge <- merge(ids, a, by.x="IID", by.y="IID", sort=F)
-
-write.table(merge, file=out_file, row=F, col=T, qu=F)
+write.table(merge[, c("FID", "IID", cpg_id), with = FALSE],
+            file = out_file,
+            row.names = FALSE,
+            col.names = TRUE,
+            quote = FALSE)
