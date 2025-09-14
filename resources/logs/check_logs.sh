b5e276b2fe4a4a7adc78425eda229adc29cab673
Haotian2020 <er20212@bristol.ac.uk>
2025-09-14 17:58:27 +0100

    1st version; adapt for deep

--- resources/logs/check_logs.sh
+++ resources/logs/check_logs.sh
@@ -43,7 +43,7 @@ vercomp () {
 compare_version () {
 
     logfile="section_${1}_logfile"
-    version_used=$(grep "GoDMC2 version" ${!logfile}* | head -n 1 | cut -d " " -f 3)
+    version_used=$(grep "DEEP version" ${!logfile}* | head -n 1 | cut -d " " -f 3)
 	if [ "${version_used}" = "" ]
 	then
 		echo ""
@@ -63,271 +63,45 @@ compare_version () {
 
 }
 
-
 check_logs_01 () {
 
-	compare_version "01"
+	compare_version "01a"
 	if grep -i -q "success" ${section_01_logfile}; then
-		echo "01-check_data.sh completed successfully."
+		echo "01a-check_data.sh completed successfully."
 	else
-		echo "Problem: 01-check_data.sh did not complete successfully"
+		echo "Problem: 01a-check_data.sh did not complete successfully"
 		exit 1
 	fi
 
-}
-
-
-check_logs_02 () {
-
-	compare_version "02a"
-	if grep -i -q "Successfully" ${section_02a_logfile}; then
-		echo "02a-snp_data.sh completed successfully."
+	compare_version "01b"
+	if grep -i -q "success" ${section_01b_logfile}; then
+		echo "01b-process_genetic_data.sh completed successfully."
 	else
-		echo "Problem: 02a-snp_data.sh did not complete successfully"
-		echo "Please ensure previous steps have been performed and rerun 02a-snp_data.sh"
+		echo "Problem: 01b-process_genetic_data.sh did not complete successfully"
 		exit 1
 	fi
 
-	compare_version "02b"
-	if grep -i -q "Successfully" ${section_02b_logfile}; then
-		echo "02b-convert_snp_format.sh completed successfully."
+	compare_version "01c"
+	if grep -i -q "success" ${section_01c_logfile}; then
+		echo "01c-check_phenotypes_and_methylation.sh completed successfully."
 	else
-		echo "Problem: 02b-convert_snp_format.sh did not complete successfully"
-		echo "Please ensure previous steps have been performed and rerun 02b-convert_snp_format.sh"
+		echo "Problem: 01c-check_phenotypes_and_methylation.sh did not complete successfully"
 		exit 1
 	fi
 
-}
-
-check_logs_03a () {
-
-  compare_version "03a"
-	if grep -i -q "success" ${section_03a_logfile}; then
-		echo "03a-methylation_variables.sh completed successfully."
+	compare_version "01d"
+	if grep -i -q "success" ${section_01d_logfile}; then
+		echo "01d-mqtl_controls.sh completed successfully."
 	else
-		echo "Problem: 03a-methylation_variables.sh did not complete successfully"
+		echo "Problem: 01d-mqtl_controls.sh did not complete successfully"
+		exit 1
+	fi
+
+	compare_version "01e"
+	if grep -i -q "success" ${section_01e_logfile}; then
+		echo "01e-genetic_pc_gwas.sh completed successfully."
+	else
+		echo "Problem: 01e-genetic_pc_gwas.sh did not complete successfully"
 		exit 1
 	fi
 }
-
-
-check_logs_03d () {
-    compare_version "03d"
-    if grep -i -q "success" ${section_03d_logfile}; then
-        echo "03d-non_genetic_methylation_pcs.sh completed successfully."
-    else
-        echo "Problem: 03d-non_genetic_methylation_pcs.sh did not complete successfully"
-        exit 1
-    fi
-}
-
-check_logs_03 () {
-
-    compare_version "03a"
-	if grep -i -q "success" ${section_03a_logfile}; then
-		echo "03a-methylation_variables.sh completed successfully."
-	else
-		echo "Problem: 03a-methylation_variables.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "03b"
-	count_03b=`grep -i "success" ${section_03b_logfile}[0-9]* | wc -l`
-	if [ ${count_03b} == ${meth_chunks} ]; then
-		echo "03b-methylation_adjustment1.sh completed successfully."
-	else
-		echo "Problem: 03b-methylation_adjustment1.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "03c"
-	if grep -i -q "success" ${section_03c_logfile}; then
-		echo "03c-methylation_pcs.sh completed successfully."
-	else
-		echo "Problem: 03c-methylation_pcs.sh did not complete successfully"
-		exit 1
-	fi
-
-    compare_version "03d"
-	if grep -i -q "success" ${section_03d_logfile}; then
-		echo "03d-non_genetic_methylation_pcs.sh completed successfully."
-	else
-		echo "Problem: 03d-non_genetic_methylation_pcs.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "03e"
-	count_03e=`grep "Successfully adjusting non-genetic PCs" ${section_03e_logfile}[0-9]* | wc -l`
-	if [ ${count_03e} == ${meth_chunks} ]; then
-		echo "03e-methylation_adjustment2.sh completed successfully."
-	else
-		echo "Problem: 03e-methylation_adjustment2.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "03f"
-	if grep -i -q "success" ${section_03f_logfile}*; then
-		echo "03f-convert_methylation_format.sh completed successfully."
-	else
-		echo "Problem: 03f-convert_methylation_format.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "03g"
-	if grep -i -q "success" ${section_03g_logfile}*; then
-		echo "03g-perform_positive_control.sh completed successfully."
-	else
-		echo "Problem: 03g-perform_positive_control.sh did not complete successfully"
-		exit 1
-	fi
-
-}
-
-check_logs_04 () {
-
-    compare_version "04a"
-	if grep -i -q "Successfully" ${section_04a_logfile}; then
-		echo "04a-convert_snp_format.sh completed successfully."
-	else
-		echo "Problem: 04a-convert_snp_format.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "04b"
-	if grep -i -q "Allele positions inverted" ${section_04b_logfile}; then
-		echo "04b-mapper-preparation.sh completed successfully."
-	else
-		echo "Problem: 04b-mapper-preparation.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "04c"
-	if grep -i -q "Successfully" ${section_04c_logfile}; then
-		echo "04c-create_mapper_files.sh completed successfully."
-	else
-		echo "Problem: 04c-create_mapper_files.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "04d"
-	if grep -i -q "Successfully" ${section_04d_logfile}; then
-		echo "04d-encoding.sh completed successfully."
-	else
-		echo "Problem: 04d-encoding.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "04e"
-	if grep -i -q "success" ${section_04e_logfile}; then
-		echo "04e-single-site-analysis.sh completed successfully."
-	else
-		echo "Problem: 04e-single-site-analysis.sh did not complete successfully"
-		exit 1
-	fi
-
-    compare_version "04f"
-    if grep -i -q "Successfully created results archives of module 04" ${section_04f_logfile}; then
-        echo "04f-tar_results.sh completed successfully."
-    else
-        echo "Problem: 04f-tar_results.sh did not complete successfully"
-        exit 1
-    fi
-}
-
-check_logs_07 () {
-
-	compare_version "07a"
-	if grep -i -q "Success" ${section_07a_logfile}; then
-		echo "07a-vmeQTL_data_preparation.sh completed successfully."
-	else
-		echo "Problem: 07a-vmeQTL_data_preparation.sh did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "07c"
-	if grep -i -q "using drm method to detect vmeQTLs is successful" ${section_07c_logfile}_drm &
-	   grep -i -q "using svlm method to detect vmeQTLs is successful" ${section_07c_logfile}_svlm &
-	   grep -i -q "using BF method to detect vmeQTLs is successful" ${section_07c_logfile}_BF; then
-		echo "07b-run_cis_vmeQTL.sh and 07c-check_vmeQTL_results.sh completed successfully."
-	else
-		echo "Problem: 07c-check_vmeQTL_results.sh did not complete successfully"
-		exit 1
-	fi
-    
-    compare_version "07d"
-    if grep -i -q "Successfully created results archives of module 07" ${section_07d_logfile}; then
-        echo "07d-tar_results.sh completed successfully."
-    else
-        echo "Problem: 07d-tar_results.sh did not complete successfully"
-        exit 1
-    fi
-}
-
-check_logs_08 () {
-
-	compare_version "08a"
-	if grep -i -q "Success" ${section_08a_logfile}; then
-		echo "08a-genotypeInversion completed successfully."
-	else
-		echo "Problem: 08a-genotypeInversion did not complete successfully"
-		exit 1
-	fi
-
-	compare_version "08b"
-	if grep -i -q "Success" ${section_08b_logfile}; then
-		echo "08b-inversionmeQTL.sh completed successfully."
-	else
-		echo "Problem: 08b-inversionmeQTL.sh did not complete successfully"
-		exit 1
-	fi
-
-
-}
-
-check_logs_14 () {
-
-	compare_version "14"
-	if grep -i -q "Success" ${section_14_logfile}; then
-		echo "14-nc886_gwas.sh completed successfully."
-	else
-		echo "Problem: 14-nc886_gwas.sh did not complete successfully"
-		exit 1
-	fi
-
-}
-
-
-check_logs_09 () {
-
-  compare_version "09"
-   
-  vect_PRS=$(grep "PRS" ${scripts_directory}/resources/parameters | grep "weights" | awk -F"_" '{print $2}' |tr "\n" " ")
-  vect_PRS_array=($vect_PRS)
-  n=$((${#vect_PRS_array[*]}-1))
-
-  for ((k=0;k<=$n;k++))
-  do
-
-    PRS=${vect_PRS_array[$k]}
-    log_file=${section_09_dir}/${PRS}/logs/log.txt
- 
-	  if grep -i -q "run successfully" ${log_file}; then
-		  echo "09 completed successfully for ${PRS}"
-	  else
-		  echo "Problem: 09 did not complete successfully for ${PRS}"
-		  exit 1
-	  fi
-
-  done
-
-	if grep -i -q "script finalised" ${section_09_logfile}; then
-		echo "09 completed successfully for all traits"
-	else
-		echo "Problem: 09 did not complete successfully for all traits"
-		exit 1
-	fi
-
-
-}
-
-
--- resources/logs/check_results.sh
+++ resources/logs/check_results.sh
@@ -1,62 +1,60 @@
 #!/usr/bin/env bash
 
-check_results_01 () {
+check_results_01a () {
 
 	if [ -f "${cohort_descriptives}" ]; then
 		echo "Cohort descriptives file present"
 	else
-		echo "Cohort descriptives file absent. Please re-run."
+		echo "Cohort descriptives file is absent."
 		exit 1
 	fi
 
-	#if [ -f "${methylation_summary}" ]; then
-	#	echo "Methylation summary file present"
-	#else
-	#	echo "Methylation summary file absent. Please re-run."
-	#	exit 1
-	#fi
-
+	if [ -f "${ageplot}" ]; then
+		echo "Age distribution plot file present"
+	else
+		echo "Age distribution plot file is absent."
+		exit 1
+	fi
 }
 
+check_results_01b () {
 
-check_results_02 () {
-
-	if [ -f "${allele_ref}" ]; then
-		echo "Allele reference file present"
+	if [ -f "${section_01_dir}/inferred_build.txt" ]; then
+		echo "Inferred build file present"
 	else
-		echo "Problem: Allele reference file is absent"
+		echo "Problem: inferred build file is absent"
 		exit 1
 	fi
 
-	if [ -f "${section_02_dir}/pcaplot.pdf" ]; then
+	if [ -f "${section_01_dir}/pcaplot.pdf" ]; then
 		echo "PCA plot present"
 	else
 		echo "Problem: PCA plot is absent"
 		exit 1
 	fi
 
-	if [ -f "${section_02_dir}/easyQC_topmed.multi.AFCHECK.png" ]; then
+	if [ -f "${section_01_dir}/easyQC_topmed.multi.AFCHECK.png" ]; then
 		echo "easyQC plot present"
 	else
 		echo "Problem: easyQC plot is absent"
 		exit 1
 	fi
 
-	if [ -f "${section_02_dir}/data.afreq.gz" ]; then
+	if [ -f "${section_01_dir}/data.afreq.gz" ]; then
 		echo "Allele frequency file present"
 	else
 		echo "Problem: Allele frequency file is absent"
 		exit 1
 	fi
 
-	if [ -f "${section_02_dir}/data.hardy.gz" ]; then
+	if [ -f "${section_01_dir}/data.hardy.gz" ]; then
 		echo "HWE file present"
 	else
 		echo "Problem: HWE file is absent"
 		exit 1
 	fi
 
-	if [ -f "${section_02_dir}/data.info.gz" ]; then
+	if [ -f "${section_01_dir}/data.info.gz" ]; then
 		echo "Imputation quality file present"
 	else
 		echo "Problem: Imputation quality file is absent"
@@ -65,16 +63,16 @@ check_results_02 () {
 
 }
 
-check_results_03a () {
+check_results_01c () {
 
-	if [ -f "${section_03_dir}/methylation_summary.RData" ]; then
+	if [ -f "${section_01_dir}/methylation_summary.RData" ]; then
 		echo "Methylation_summary.RData is present"
 	else
 		echo "Problem: methylation_summary.RData is absent"
 		exit 1
 	fi
 
-    if [ -f "${section_03_dir}/cohort_descriptives_commonids.RData" ]; then
+    if [ -f "${section_01_dir}/cohort_descriptives_commonids.RData" ]; then
 		echo "cohort_descriptives_commonids.RData is present"
 	else
 		echo "Problem: cohort_descriptives_commonids.RData is absent"
@@ -82,30 +80,29 @@ check_results_03a () {
 	fi
 
 	
-	if [ -f "${section_03_dir}/cellcounts_summary.txt" ]; then
+	if [ -f "${section_01_dir}/cellcounts_summary.txt" ]; then
 		echo "Summary statistics of cell counts are present"
 	else
 		echo "Problem: summary statistics of cell counts are absent"
 		exit 1
 	fi
 
-	if [ -f "${section_03_dir}/cellcounts_plot.pdf" ]; then
+	if [ -f "${section_01_dir}/cellcounts_plot.pdf" ]; then
 		echo "Plots of cell counts are present"
 	else
 		echo "Problem: plots of cell counts are absent"
 		exit 1
 	fi
 
-
 	if [ "${measured_cellcounts}" != "NULL" ];then
-		if [  -f "${section_03_dir}/cor_plot.pdf" ]; then
+		if [  -f "${section_01_dir}/cor_plot.pdf" ]; then
 			echo "Correlation plot of observed vs predicted cell counts is present"
 		else
 			echo "Problem: correlation plot of observed vs predicted cell counts is absent"
 			exit 1
         fi        
 
-		if [  -f "${section_03_dir}/cor_matrix.txt" ]; then
+		if [  -f "${section_01_dir}/cor_matrix.txt" ]; then
 			echo "Correlation matrix of observed vs predicted cell counts is present"
 		else
 			echo "Problem: correlation matrix of observed vs predicted cell counts is absent"
@@ -123,284 +120,124 @@ check_results_03a () {
 		exit 1
 	fi
 
-	if [ -f "${section_03_dir}/age_prediction.pdf" ]; then
-		echo "The correlation plot between predicted and actual ages is present"
-	else
-		echo "Problem: The correlation plot between predicted and actual ages is absent"
-		exit 1
-	fi
-
-	if [ -f "${section_03_dir}/age_prediction_correlation.png" ]; then
+	if [ -f "${section_01_dir}/age_prediction_correlation.png" ]; then
 		echo "The matrix correlation plot among predicted age, age acceleration residuals, and chronological age is present"
 	else
 		echo "Problem: The matrix correlation plot of predicted age is absent"
 		exit 1
 	fi
 
-	if [ -f "${section_03_dir}/age_prediction_stats.csv" ]; then
-		echo "The statistical table for each clock and their age acceleration modules is present"
-	else
-		echo "Problem: The statistical table for each clock and their age acceleration modules is absent"
-		exit 1
-	fi
-
-	if [ -f "${section_03_dir}/age_prediction_stats_corrsd.csv" ]; then
-		echo "The statistical table for each pair of comparisons in the matrix plot of aging is present"
-	else
-		echo "Problem: The statistical table for each pair of comparisons in the matrix plot of aging is absent"
-		exit 1
-	fi
-
 }
 
-check_results_03d () {
-    	echo "The number of methylation files may varied across cohorts. For more details, please check the Wiki. Please ensure you have seen the scripts 03a-03d run successfully from log files."
-}
+# ...existing code...
 
-check_results_03 () {
+check_results_01d () {
 
-	check_results_03a
-
- 	check_results_03d
-
- 	if [ -f "${section_03_dir}/positive_control_transformed_${positive_control_cpg}.PHENO1.glm.linear.gz" ]; then
-		echo "transformed mQTL analysis positive control results present"
-	else
-		echo "transformed mQTL analysis positive control results file not present"
-		exit 1
-	fi
-
-	if [ -f "${section_03_dir}/positive_control_transformed_${positive_control_cpg}_manhattan.pdf" ]; then
-		echo "transformed mQTL analysis positive control Manhattan plot present"
-	else
-		echo "transformed mQTL analysis positive control Manhattan plot file not present"
-		exit 1
-	fi
-
-		if [ -f "${section_03_dir}/positive_control_transformed_${positive_control_cpg}_nocisChr_manhattan.pdf" ]; then
-		echo "transformed mQTL analysis positive control Manhattan plot present"
-	else
-		echo "transformed mQTL analysis positive control Manhattan plot file not present"
-		exit 1
-	fi
-
-	if [ -f "${section_03_dir}/positive_control_transformed_${positive_control_cpg}_qqplot.jpeg" ]; then
-		echo "transformed mQTL analysis positive control QQ plot present"
-	else
-		echo "transformed mQTL analysis positive control QQ plot file not present"
-		exit 1
-	fi
-
-		if [ -f "${section_03_dir}/positive_control_transformed_${positive_control_cpg}_nocisChr_qqplot.jpeg" ]; then
-		echo "transformed mQTL analysis positive control QQ plot present"
-	else
-		echo "transformed mQTL analysis positive control QQ plot file not present"
-		exit 1
-	fi
-
-	if [ -f "${section_03_dir}/positive_control_untransformed_${positive_control_cpg}.PHENO1.glm.linear.gz" ]; then
-		echo "untransformed analysis positive control results present"
-	else
-		echo "untransformed analysis positive control results file not present"
-		exit 1
-	fi
-	
-	if [ -f "${section_03_dir}/positive_control_untransformed_${positive_control_cpg}_manhattan.pdf" ]; then
-		echo "untransformed analysis positive control Manhattan plot present"
-	else
-		echo "untransformed analysis positive control Manhattan plot file not present"
-		exit 1
-	fi
-
-		if [ -f "${section_03_dir}/positive_control_untransformed_${positive_control_cpg}_nocisChr_manhattan.pdf" ]; then
-		echo "untransformed analysis positive control Manhattan plot present"
-	else
-		echo "untransformed analysis positive control Manhattan plot file not present"
-		exit 1
-	fi
-	
-	if [ -f "${section_03_dir}/positive_control_untransformed_${positive_control_cpg}_qqplot.jpeg" ]; then
-		echo "untransformed analysis positive control QQ plot present"
-	else
-		echo "untransformed analysis positive control QQ plot file not present"
-		exit 1
-	fi
-
-		if [ -f "${section_03_dir}/positive_control_untransformed_${positive_control_cpg}_nocisChr_qqplot.jpeg" ]; then
-		echo "untransformed analysis positive control QQ plot present"
-	else
-		echo "untransformed analysis positive control QQ plot file not present"
-		exit 1
-	fi
-	
-}
-
-check_results_04 () {
-
-	if [ -f "${home_directory}/results/04/meta_inputs/part_dev/${study_name}_a_cov.npy" ]; then
-		echo "${study_name}_a_cov.npy present"
-	else
-		echo "${study_name}_a_cov.npy absent. Please re-run."
-		exit 1
-	fi
-
-	if [ -f "${home_directory}/results/04/meta_inputs/part_dev/${study_name}_C.npy" ]; then
-		echo "${study_name}_C.npy present"
-	else
-		echo "${study_name}_C.npy absent. Please re-run."
-		exit 1
-	fi
-
-	if [ -f "${home_directory}/results/04/meta_inputs/part_dev/${study_name}_b_cov.npy" ]; then
-		echo "${study_name}_b_cov.npy present"
-	else
-		echo "${study_name}_b_cov.npy absent. Please re-run."
-		exit 1
-	fi
-
-	if [ -f "${home_directory}/results/04/meta_inputs/part_dev/${study_name}_a_test.npy" ]; then
-		echo "${study_name}_a_test.npy present"
-	else
-		echo "${study_name}_a_test.npy absent. Please re-run."
-		exit 1
-	fi
-
-	if [ -f "${home_directory}/results/04/meta_inputs/part_dev/${study_name}_metadata.npy" ]; then
-		echo "${study_name}_metadata.npy present"
-	else
-		echo "${study_name}_metadata.npy absent. Please re-run."
-		exit 1
-	fi
-
-    if [ -f "${home_directory}/results/${study_name}_04.tgz" ]; then
-        echo "hase tar results present"
-    else
-        echo "hase tar results absent. Please re-run"
+    if [ ! -f "${methylation_processed_dir}/mqtl_pos_ctr_filt.tsv" ]; then
+        echo "Problem: filtered positive control file is absent: ${methylation_processed_dir}/mqtl_pos_ctr_filt.tsv"
+        exit 1
     fi
 
-}
+	echo "Filtered positive control file is present: ${methylation_processed_dir}/mqtl_pos_ctr_filt.tsv"
+    while IFS=$'\t' read -r positive_control_cpg _; do
+        [ -z "${positive_control_cpg}" ] && continue
 
+        echo "Checking positive control: ${positive_control_cpg}"
 
-check_results_07 () {
+        if [ -f "${section_01_dir}/positive_control_untransformed_${positive_control_cpg}.fastGWAmlm.gz" ]; then
+            echo "positive control ${positive_control_cpg} results present"
+        else
+            echo "Problem: positive control ${positive_control_cpg} results file not present: ${section_01_dir}/positive_control_untransformed_${positive_control_cpg}.fastGWAmlm.gz"
+			exit 1
+        fi
 
-    chunk_count=`cat ${section_07_dir}/tabfile.info1 | wc -l`
-    BFfile=`ls ${section_07_dir}/vQTL_BF_*besd | wc -l`
-    svlmfile=`ls ${section_07_dir}/vQTL_BF_*besd | wc -l`
-    drmfile=`ls ${section_07_dir}/vQTL_BF_*besd | wc -l`
+        if [ -f "${section_01_dir}/positive_control_untransformed_${positive_control_cpg}_manhattan.pdf" ]; then
+            echo "positive control ${positive_control_cpg} Manhattan plot present"
+        else
+            echo "Problem: positive control ${positive_control_cpg} Manhattan plot file not present"
+			exit 1
+        fi
 
-    if [ $chunk_count = $BFfile ]; then
-        echo "vQTL detection results with BF method present"
-    fi
+        if [ -f "${section_01_dir}/positive_control_untransformed_${positive_control_cpg}_nocisChr_manhattan.pdf" ]; then
+            echo "positive control ${positive_control_cpg} no cis chromosome Manhattan plot present"
+        else
+            echo "Problem: positive control ${positive_control_cpg} no cis chromosome Manhattan plot file not present"
+			exit 1
+        fi
 
-    if [ $chunk_count = $svlmfile ]; then
-        echo "vQTL detection results with svlm method present"
-    fi 
+        if [ -f "${section_01_dir}/positive_control_untransformed_${positive_control_cpg}_qqplot.jpeg" ]; then
+            echo "positive control ${positive_control_cpg} QQ plot present"
+        else
+            echo "Problem: positive control ${positive_control_cpg} QQ plot file not present"
+			exit 1
+        fi
 
-    if [ $chunk_count = $drmfile ]; then
-        echo "vQTL detection results with drm method present"
-    fi
+        if [ -f "${section_01_dir}/positive_control_untransformed_${positive_control_cpg}_nocisChr_qqplot.jpeg" ]; then
+            echo "positive control ${positive_control_cpg} no cis chromosome QQ plot present"
+        else
+            echo "Problem: positive control ${positive_control_cpg} no cis chromosome QQ plot file not present"
+	exit 1
+        fi
 
-    if [ -f "${home_directory}/results/${study_name}_07.tgz" ]; then
-        echo "vQTL tar results present"
-    else
-        echo "vQTL tar results absent. Please re-run"
-    fi
-}
+        # negative control corresponding name: NEG_${positive_control_cpg}
+        neg="NEG_${positive_control_cpg}"
+        echo "Checking negative control: ${neg}"
 
+        if [ -f "${section_01_dir}/negative_control_untransformed_${neg}.fastGWAmlm.gz" ]; then
+            echo "negative control ${neg} results present"
+        else
+            echo "Problem: negative control ${neg} results file not present: ${section_01_dir}/negative_control_untransformed_${neg}.fastGWAmlm.gz"
+            exit 1
+        fi
 
-check_results_08 () {
+        if [ -f "${section_01_dir}/negative_control_untransformed_${neg}_manhattan.pdf" ]; then
+            echo "negative control ${neg} Manhattan plot present"
+        else
+            echo "Problem: negative control ${neg} Manhattan plot file not present"
+            exit 1
+        fi
 
-	if [ -f "${section_08_dir}/badInversions.txt" ]; then
-		echo "Bad inversions file present"
-	else
-		echo "Problem: Bad inversions file is absent"
-		exit 1
-	fi
+        if [ -f "${section_01_dir}/negative_control_untransformed_${neg}_nocisChr_manhattan.pdf" ]; then
+            echo "negative control ${neg} no cis chromosome Manhattan plot present"
+        else
+            echo "Problem: negative control ${neg} no cis chromosome Manhattan plot file not present"
+            exit 1
+        fi
 
-	if [ -f "${section_08_dir}/inversionsSummary.txt" ]; then
-		echo "Inversion frequency file present"
-	else
-		echo "Problem: Inversion frequency file is absent"
-		exit 1
-	fi
+        if [ -f "${section_01_dir}/negative_control_untransformed_${neg}_qqplot.jpeg" ]; then
+            echo "negative control ${neg} QQ plot present"
+        else
+            echo "Problem: negative control ${neg} QQ plot file not present"
+            exit 1
+        fi
 
-	if [ -f "${section_08_dir}/invmeqtl.Rdata" ]; then
-		echo "inversionmeQTL statistics file present"
-	else
-		echo "Problem: inversionmeQTL statistics file is absent"
-		exit 1
-	fi
+        if [ -f "${section_01_dir}/negative_control_untransformed_${neg}_nocisChr_qqplot.jpeg" ]; then
+            echo "negative control ${neg} no cis chromosome QQ plot present"
+        else
+            echo "Problem: negative control ${neg} no cis chromosome QQ plot file not present"
+			exit 1
+        fi
 
-}
-
-
-
-check_results_09 () {
-
-
-  vect_PRS=$(grep "PRS" ${scripts_directory}/resources/parameters | grep "weights" | awk -F"_" '{print $2}' |tr "\n" " ")
-  vect_PRS_array=($vect_PRS)
-
-  n=$((${#vect_PRS_array[*]}-1))
-
-  for ((k=0;k<=$n;k++))
-  do
-    PRS=${vect_PRS_array[$k]}
-    PRS_dir=${section_09_dir}/${PRS}
-   
-	  if [ -f "${section_09_dir}/${PRS}/${study_name}_PRS_${PRS}_hist.pdf" ]; then
-		  echo "Histogram for $PRS PRS present"
-	  else
-		  echo "Problem: histogram for $PRS PRS is absent"
-		  exit 1
-	  fi
-
-	  if [ -f "${section_09_dir}/${PRS}/${study_name}_PRS_${PRS}_cell_counts_plots.pdf" ]; then
-		  echo "Cell count PRS correlation plot for $PRS PRS present"
-	  else
-		  echo "Problem: cell count correlation plot for $PRS PRS is absent"
-		  exit 1
-	  fi
-
-	  if [ -f "${section_09_dir}/${PRS}/${study_name}_PRS_${PRS}_EWAS_results.RData" ]; then
-		  echo "EWAS results file for $PRS PRS present"
-	  else
-		  echo "Problem: EWAS results file for $PRS PRS is absent"
-		  exit 1
-	  fi
-
-	  if [ -f "${section_09_dir}/${PRS}/${study_name}_PRS_${PRS}_EWAS.ewas.report.html" ]; then
-		  echo "EWAS report file for $PRS PRS present"
-	  else
-		  echo "Problem: EWAS report file for $PRS PRS is absent"
-		  exit 1
-	  fi
-
-		 if [ -f "${section_09_dir}/${PRS}/${study_name}_PRS_${PRS}_EWAS_qqplot.isvacovs.pdf" ] && [ -f "${section_09_dir}/${PRS}/${study_name}_PRS_${PRS}_EWAS_qqplot.nocovs.pdf" ]; then
-		
-      echo "QQplots for $PRS PRS EWAS present"
-	  else
-		  echo "Problem: not all QQplots for $PRS PRS EWAS present"
-		  exit 1
-	  fi
-
-  done
+    done < <(awk -F'\t' 'NR>1 { gsub(/^[ \t]+|[ \t]+$/,"",$1); if($1!="" && $1!~ /^#/) print $1 }' "${methylation_processed_dir}/mqtl_pos_ctr_filt.tsv")
 
 }
 
-check_results_14 () {
+check_results_01e () {
+    for i in {1..5}; do
+        pc="PC${i}"
 
-	if [ -f "${section_14_dir}/nc886_scatter.jpeg" ]; then
-		echo "scatterplot present"
-	else
-		echo "Problem: Scatterplot is absent"
-		exit 1
-	fi
+        if [ -f "${section_01_dir}/gwas_${pc}.fastGWAmlm.gz" ]; then
+            echo "GWAS ${pc} results present"
+        else
+            echo "Problem: GWAS ${pc} results file not present: ${section_01_dir}/gwas_${pc}.fastGWAmlm.gz"
+            exit 1
+        fi
 
-	if [ -f "${section_14_dir}/nc886_frequency.txt" ]; then
-		echo "nc866 frequency file present"
-	else
-		echo "Problem: nc866 frequency file is absent"
-		exit 1
-	fi
-	}
+        if [ -f "${section_01_dir}/gwas_${pc}_manhattan_beta.pdf" ]; then
+            echo "GWAS ${pc} beta Manhattan plot present"
+        else
+            echo "Problem: GWAS ${pc} beta Manhattan plot file not present: ${section_01_dir}/gwas_${pc}_manhattan_beta.pdf"
+            exit 1
+        fi
+    done
+}
