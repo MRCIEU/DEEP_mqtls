3ba9caf00a9dcb8b0898908035aa0ca8581fd2a8
Haotian2020 <er20212@bristol.ac.uk>
2025-09-25 16:43:07 +0100

    message for moving

--- 01b-process_genetic_data.sh
+++ 01b-process_genetic_data.sh
@@ -446,6 +446,7 @@ ${plink2} \
 
 gzip -f -c ${quality_scores} > ${section_01_dir}/data.info.gz
 gzip ${section_01_dir}/data.hardy
+echo "Moving smiss file to processed_data/genetic_data"
 mv ${section_01_dir}/data.smiss ${home_directory}/processed_data/genetic_data/data.smiss
 gzip ${home_directory}/processed_data/genetic_data/data.smiss
 gzip ${section_01_dir}/data.vmiss
