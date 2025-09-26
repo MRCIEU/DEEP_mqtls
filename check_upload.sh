020f244da70041ad90df27e37666a40365f24ee8
Haotian2020 <er20212@bristol.ac.uk>
2025-09-26 17:53:48 +0100

    update check_upload

--- check_upload.sh
+++ check_upload.sh
@@ -41,8 +41,7 @@ eval "check_results_$1"
 echo ""
 echo "Section $1 has been successfully completed!"
 
-if [[ "$2" = "upload" && ( $1 = "01" ) ]]
-then
+if [[ "$2" = "upload" && ( $1 = "01" ) ]]; then
 
 	echo ""
 	temp=`which sshpass 2>/dev/null | wc -l`
@@ -51,14 +50,6 @@ then
 	then
 		echo "sshpass detected"
 		sftp $port -oIdentityFile=$key -oBatchMode=no -b - ${sftp_username}@${sftp_address}:${sftp_path} << !
-bye
-!
-	elif [[ ! "${temp}" = "0" && $1 = "08" ]]
-	then
-		read -s -p "Enter SFTP password: " mypassword
-	    export SSHPASS=${mypassword}
-		sshpass -e sftp -P 22 -oIdentityFile=$key -oBatchMode=no -b - ${sftp_username_inversions}@${sftp_address_inversions}:${sftp_path_inversions} << !
-	
 bye
 !
 		echo "Connection established"
@@ -71,13 +62,18 @@ bye
 	echo "Tarring results, log and config files"
 	mkdir -p ${home_directory}/results/config/
 	if [[ "$config_file" = /* ]]; then
-		cp ${config_file} ${scripts_directory}/
-		config_basename=$(basename "$config_file")
-		tar czf ${home_directory}/results/config/${study_name}_config.tar -C ${scripts_directory}/ ./${config_basename} ./resources/parameters
-		rm ${scripts_directory}/${config_basename}
+    	target="${scripts_directory}/$(basename "$config_file")"
+    	if [[ "${config_file}" != "$target" ]]; then
+        cp ${config_file} ${scripts_directory}/
+    	fi
+    	config_basename=$(basename "$config_file")
+    	tar czf ${home_directory}/results/config/${study_name}_config.tar -C ${scripts_directory}/ ./${config_basename} ./resources/parameters
+    	if [[ "${config_file}" != "$target" ]]; then
+        	rm ${scripts_directory}/${config_basename}
+    	fi
 	else
-		tar czf ${home_directory}/results/config/${study_name}_config.tar -C ${scripts_directory}/ ./${config_file} ./resources/parameters 
-	fi	
+    	tar czf ${home_directory}/results/config/${study_name}_config.tar -C ${scripts_directory}/ ./${config_file} ./resources/parameters 
+	fi
     echo "Successfully created config archive"	
 
     echo "Generating md5 checksum"
@@ -85,58 +81,18 @@ bye
     echo "Encrypting files"
     gpg --output ${home_directory}/results/config/${study_name}_config.tar.aes --symmetric --cipher-algo AES256 ${home_directory}/results/config/${study_name}_config.tar
     echo ""
-
-	if [ "${1}" = "17" ]
-	then
-		suff="tar"
-		flags="cf"
-	else
-		suff="tgz"
-		flags="czf"
-	fi
-
-    if [[ $1 = "03a" || $1 = "03d" ]]
-    then
-        tar ${flags} ${home_directory}/results/${study_name}_${1}.${suff} -C ${home_directory} results/03/
-        echo "Successfully created results archives"
-#        echo "Generating md5 checksum"
-#        md5sum ${home_directory}/results/${study_name}_${1}.${suff} > ${home_directory}/results/${study_name}_${1}.md5sum
-#        echo "Encrypting files"
-#        gpg --output ${home_directory}/results/${study_name}_${1}.${suff}.aes --symmetric --cipher-algo AES256 ${home_directory}//results/${study_name}_${1}.${suff}
-        echo ""
-    elif [[ $1 = "07" || $1 = "04" ]]
-    then
-        echo "Tarring results have been generated"
-    else
-	    tar ${flags} ${home_directory}/results/${study_name}_${1}.${suff} -C ${home_directory} results/${1}
-        echo "Successfully created results archives"
-    fi
-    
+	
+	suff="tgz"
+	flags="czf"
+    tar ${flags} ${home_directory}/results/${study_name}_${1}.${suff} -C ${home_directory} results/${1}
     echo "Generating md5 checksum"
     md5sum ${home_directory}/results/${study_name}_${1}.${suff} > ${home_directory}/results/${study_name}_${1}.md5sum
     echo "Encrypting files"
-    gpg --output ${home_directory}/results/${study_name}_${1}.${suff}.aes --symmetric --cipher-algo AES256 ${home_directory}//results/${study_name}_${1}.${suff}
+    gpg --output ${home_directory}/results/${study_name}_${1}.${suff}.aes --symmetric --cipher-algo AES256 ${home_directory}/results/${study_name}_${1}.${suff}
     echo ""
 
-
-if [[ ! "${temp}" = "0"  ]]
-then
-echo "Detecting sshpass"
-
-	if [[  $1 = "08" ]]
-	then
-		echo "Inversions"
-		export SSHPASS=${mypassword}
-	    sshpass -e sftp -P 22 -oIdentityFile=$key -oBatchMode=no -b - ${sftp_username_inversions}@${sftp_address_inversions}:${sftp_path_inversions} << !
-   		dir
-		cd ${sftp_username_inversions}
-   		put ${home_directory}/results/${study_name}_${1}.md5sum
-   		put ${home_directory}/results/${study_name}_$1.${suff}.aes
-		put ${home_directory}/results/config/${study_name}_config.tar.aes
-		put ${home_directory}/results/config/${study_name}_config.md5sum 
-   		bye
-!
-    else
+	if [[ ! "${temp}" = "0"  ]]; then
+	echo "Detecting sshpass"
         sftp $port -oIdentityFile=$key -oBatchMode=no -b - ${sftp_username}@${sftp_address}:${sftp_path} << !
         dir
         cd ../upload
@@ -146,21 +102,5 @@ echo "Detecting sshpass"
 		put ${home_directory}/results/config/${study_name}_config.md5sum 
         bye
 !
-	fi
-else
-
-read -s -p "Ready to upload? Press enter to continue: " anykey
-
-sftp $port -oIdentityFile=$key ${sftp_username}@${sftp_address}:${sftp_path} <<EOF
-
-    dir
-    cd ../upload
-    put ${home_directory}/results/${study_name}_${1}.md5sum
-    put ${home_directory}/results/${study_name}_$1.${suff}.aes
-	put ${home_directory}/results/config/${study_name}_config.tar.aes
-	put ${home_directory}/results/config/${study_name}_config.md5sum 
-EOF
-
 fi
-
 fi
