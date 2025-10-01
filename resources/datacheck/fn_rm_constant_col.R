b9b056328f8784369c9515e1972b8dfc9bbe3e89
Haotian2020 <er20212@bristol.ac.uk>
2025-10-01 19:48:17 +0100

    update func with threshold 0.0003

--- resources/datacheck/fn_rm_constant_col.R
+++ resources/datacheck/fn_rm_constant_col.R
@@ -1,13 +1,32 @@
-remove_constant_cols <- function(df, label) {
+remove_constant_cols <- function(df, label, threshold = 0.0003) {
   cols_to_check <- setdiff(names(df), c("FID", "IID"))
-
-  constant_cols <- cols_to_check[sapply(df[cols_to_check], function(x) length(unique(x)) == 1)]
-
-  if (length(constant_cols) > 0) {
-    message(sprintf("In %s, removing columns with no variation: %s", label, paste(constant_cols, collapse = ", ")))
-    df <- df[, !names(df) %in% constant_cols, drop = FALSE]
+  
+  col_sds <- sapply(df[cols_to_check], function(x) {
+    if (is.numeric(x)) {
+      sd(x, na.rm = TRUE)
+    } else if (is.factor(x)) {
+      if (nlevels(x) <= 1) {
+        0
+      } else { # leave for T or F
+        NA_real_
+      }
+    } else {
+      NA_real_
+    }
+  })
+  
+  low_sd_cols <- names(col_sds)[!is.na(col_sds) & col_sds <= threshold]
+  
+  message("Detected low-variance columns in ", label, ": ",
+          ifelse(length(low_sd_cols) > 0, paste(low_sd_cols, collapse = ", "), "none"))
+  
+  if (length(low_sd_cols) > 0) {
+    message(sprintf("In %s, removing columns with SD ≤ %.4f: %s",
+                    label, threshold, paste(low_sd_cols, collapse = ", ")))
+    df <- df[, !names(df) %in% low_sd_cols, drop = FALSE]
   }
   
-  message(sprintf("After filtering, %s columns: %s", label, paste(names(df), collapse = ", ")))
+  message(sprintf("After filtering, %s columns: %s",
+                  label, paste(names(df), collapse = ", ")))
   return(df)
 }
