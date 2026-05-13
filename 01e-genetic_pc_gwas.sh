964d52760c2f763ab86d50cf31de5c52e0afa4ea
Haotian2020 <er20212@bristol.ac.uk>
2026-05-13 15:23:48 +0100

    use args for gcta; grm from king/pc-relate

--- 01e-genetic_pc_gwas.sh
+++ 01e-genetic_pc_gwas.sh
@@ -8,108 +8,74 @@ print_version
 
 echo "Running GWAS for genetic PCs (PC1-PC10)"
 
-covar_option=""
+# Step 1: decide whether to use a sparse GRM and fastGWA mode; GRMs are generated in 01b
+use_sparse_grm="no"
+fastgwa_mode="lr"
+grm_sparse_prefix=""
+
+if [ "${related}" = "yes" ] && [ "${structured}" = "yes" ]; then
+    echo "Related + Structured: using PC-Relate sparse GRM from 01b"
+    fastgwa_mode="mlm"
+    use_sparse_grm="yes"
+
+    if [ -f "${grmfile_pcrelate}.grm.sp" ] && [ -f "${grmfile_pcrelate}.grm.id" ]; then
+        grm_sparse_prefix="${grmfile_pcrelate}"
+        echo "Using PC-Relate GCTA sparse GRM prefix: ${grm_sparse_prefix}"
+    else
+        echo "Error: Required PC-Relate sparse GRM files not found:"
+        echo "  ${grmfile_pcrelate}.grm.sp"
+        echo "  ${grmfile_pcrelate}.grm.id"
+        exit 1
+    fi
+
+elif [ "${related}" = "yes" ] && [ "${structured}" = "no" ]; then
+    echo "Related + non-structured: using KING sparse GRM from 01b"
+    fastgwa_mode="mlm"
+    use_sparse_grm="yes"
+    grm_sparse_prefix="${grmfile_king}_sparse"
+
+    if [ -f "${grmfile_king}_sparse.grm.sp" ] && [ -f "${grmfile_king}_sparse.grm.id" ]; then
+        echo "Using KING sparse GRM prefix: ${grm_sparse_prefix}"
+    else
+        echo "Error: Required KING sparse GRM files not found:"
+        echo "  ${grmfile_king}_sparse.grm.sp"
+        echo "  ${grmfile_king}_sparse.grm.id"
+        exit 1
+    fi
+
+elif [ "${related}" = "no" ]; then
+    echo "Unrelated sample: no GRM needed (fastGWA-lr)"
+    fastgwa_mode="lr"
+    use_sparse_grm="no"
+    grm_sparse_prefix=""
+
+else
+    echo "Error: Invalid related/structured flag combination. Set both to yes or no."
+    exit 1
+fi
+
+fastgwa_option=()
+grm_sparse_option=()
+if [ "${use_sparse_grm}" = "yes" ]; then
+    fastgwa_option=("--fastGWA-mlm")
+    grm_sparse_option=("--grm-sparse" "${grm_sparse_prefix}")
+elif [ "${fastgwa_mode}" = "lr" ]; then
+    fastgwa_option=("--fastGWA-lr")
+else
+    echo "Error: Unsupported fastGWA mode: ${fastgwa_mode}"
+    exit 1
+fi
+
+covar_args=()
 if [ -f "${ccovar_file}" ]; then
   ncols=$(awk -F'\t' 'NR==1{print NF; exit}' "${ccovar_file}" 2>/dev/null || echo 0)
   if [ "${ncols}" -gt 2 ]; then
-    covar_option="--covar ${ccovar_file}"
+    covar_args=("--covar" "${ccovar_file}")
   else
     echo "No categorical covariates detected in ${ccovar_file}; skipping --covar for GCTA."
   fi
 else
   echo "Warning: ccovar file not found: ${ccovar_file}"
-fi
-
-if [ "${related}" = "yes" ]; then
-    echo "Generating a new grm for mlm, to avoid proximal contamination"
-    echo "Sorting variants using pgen"
-    ${plink2} \
-      --bfile ${bfile} \
-      --make-pgen \
-      --sort-vars \
-      --new-id-max-allele-len 70 \
-      --out ${bfile_sort} \
-      --threads ${nthreads}
-
-    ${plink2} \
-      --pfile ${bfile_sort} \
-      --new-id-max-allele-len 70 \
-      --make-bed \
-      --out ${bfile_sort}
-
-    echo "Identifying SNPs that are highly LD with original PCA SNPs"
-    ${plink} \
-      --bfile ${bfile_sort} \
-      --r2 gz \
-      --ld-snp-list ${pca}.prune.in \
-      --new-id-max-allele-len 70 \
-      --ld-window-kb 1000 \
-      --ld-window-r2 0.1 \
-      --out "${high_ld}"
-
-    echo "Creating a list of SNPs to exclude"
-
-    zcat ${high_ld}.ld.gz |awk 'NR==1 && $6 ~ /[A-Za-z]/ {next} {print $6}' | awk 'NF' | sort -u > "${high_ld}.exclude.ids"
-    echo "Total SNPs to be excluded: $(wc -l < "${high_ld}.exclude.ids")"
-
-    echo "Excluding high LD regions in genome first"
-    ${plink2} \
-      --bfile ${bfile_sort} \
-      --exclude range ${exclude_highld_region} \
-      --new-id-max-allele-len 70 \
-      --make-bed \
-      --out ${bfile_sort}1
-
-    mv ${bfile_sort}1.bed ${bfile_sort}.bed
-		mv ${bfile_sort}1.bim ${bfile_sort}.bim
-		mv ${bfile_sort}1.fam ${bfile_sort}.fam
-
-    echo "Excluding highly-LD SNPs and pruning SNPs for GRM"
-    ${plink2} \
-      --bfile ${bfile_sort} \
-	    --new-id-max-allele-len 70 \
-	    --exclude "${high_ld}.exclude.ids" \
-      --indep-pairwise 10000 5 0.1 \
-	    --out ${snp_01e} \
-	    --autosome \
-	    --threads ${nthreads}
-
-if [ -f "${snp_01e}.prune.in" ]; then
-    snp_count=$(wc -l < "${snp_01e}.prune.in")
-    
-    echo "SNP count in snps_01e.prune.in: $snp_count"
-
-    if [ "$snp_count" -lt 50000 ]; then
-        echo "WARNING: SNP count is less than 50,000! This may lead to an unstable GRM. Consider adjusting the LD pruning parameters or excluding fewer SNPs. Please contact Haotian (haotian.tang@bristol.ac.uk)."
-    fi
-
-fi
-
-    echo "Generating new GRM for related individuals"
-    ${plink2} \
-      --bfile ${bfile} \
-	    --new-id-max-allele-len 70 \
-	    --extract "${snp_01e}.prune.in" \
-	    --make-grm-bin \
-	    --out "${grmfile_all_01e}" \
-	    --threads "${nthreads}" \
-	    --autosome
-
-    echo "Sample size in creating kinship matrix: $(wc -l < "${grmfile_all_01e}.grm.id")"
-
-    ${R_directory}Rscript resources/relateds/grm_distri.R \
-	    "${grmfile_all_01e}" \
-	    "${rel_cutoff}" \
-	    "${grm_distribution}_01e"
-
-    echo "Generating sparse GRM for fastGWA-mlm"
-    ${gcta} \
-	    --grm ${grmfile_all_01e} \
-	    --make-bK-sparse 0.05 \
-	    --autosome \
-	    --make-grm \
-	    --out ${grmfile_fast}_rel_01e \
-	    --thread-num ${nthreads}
 fi
 
 for pc in {1..5}; do
@@ -119,31 +85,23 @@ for pc in {1..5}; do
     awk -v col="$((pc+2))" 'NR==1{next} {print $1, $2, $col}' ${genetic_pc_gwas} > ${pheno_file}
 
     echo "Running GWAS for ${pc_col}"
-    if [ "${related}" = "yes" ]; then
-        ${gcta} \
-            --bfile "${bfile}" \
-            --grm-sparse "${grmfile_fast}_rel_01e" \
-            --fastGWA-mlm \
-            --h2-limit 100 \
-            --pheno "${pheno_file}" \
-            --qcovar "${qcovar_noPC_file}" \
-            ${covar_option} \
-            --covar-maxlevel 500 \
-            --out "${section_01_dir}/01e/gwas_${pc_col}" \
-            --thread-num "${nthreads}"
-
-    elif [ "${related}" = "no" ]; then
-        ${gcta} \
-            --bfile "${bfile}" \
-            --fastGWA-lr \
-            --h2-limit 100 \
-            --pheno "${pheno_file}" \
-            --qcovar "${qcovar_noPC_file}" \
-            ${covar_option} \
-            --covar-maxlevel 500 \
-            --out "${section_01_dir}/01e/gwas_${pc_col}" \
-            --thread-num "${nthreads}"
+    if [ "${use_sparse_grm}" = "yes" ]; then
+      echo "Using sparse GRM prefix: ${grm_sparse_prefix}"
+    else
+      echo "No GRM mode: fastGWA-lr"
     fi
+
+    ${gcta} \
+      --bfile "${bfile}" \
+      "${grm_sparse_option[@]}" \
+      "${fastgwa_option[@]}" \
+      --h2-limit 100 \
+      --pheno "${pheno_file}" \
+      --qcovar "${qcovar_noPC_file}" \
+      "${covar_args[@]}" \
+      --covar-maxlevel 500 \
+      --out "${section_01_dir}/01e/gwas_${pc_col}" \
+      --thread-num "${nthreads}"
 
     tr -s " " < "${section_01_dir}/01e/gwas_${pc_col}.fastGWA" | gzip -c > "${section_01_dir}/01e/gwas_${pc_col}.fastGWA.gz"
     rm "${section_01_dir}/01e/gwas_${pc_col}.fastGWA"
